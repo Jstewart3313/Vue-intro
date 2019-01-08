afed67439fac3a72f56446683655dba5369dea82
Justin Stewart <Justinstewart3313@gmail.com>
2019-01-07 19:04:16 -0700

    Refactor for new monthly design, No need for monthly component, show one picture at a time, being able to cycle through the entire month

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,74 @@
-<template >
-  <div v-if='!daily' id="app">
-    <Monthly></Monthly>
-  </div>
-  <div v-else='daily' id="app">
-    <h1>{{todaysPlanets.title}}</h1>
-    <img :src=todaysPlanets.hdurl />
-    <button v-on:click='monthly'>
-      View Monthly
-    </button>
+<template>
+  <div id="app">
+    <h1>{{ planets.title }}</h1>
+    <img :src="planets.hdurl" />
+    <button v-on:click="Next">View Next</button>
+    <button v-on:click="getPlanet">View Today</button>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import Monthly from './components/Monthly'
+  import axios from "axios";
 
-export default {
-  name: 'App',
-  components: {
-    Monthly
-  },
-  data() {
-    return {
-      todaysPlanets: {},
-      daily: true
-    }
-  },
-  created: function() {
-    // this.getPlanet()
-    this.getMocks()
-  },
-  methods: {
-    // getPlanet() {
-    //   axios.get('https://api.nasa.gov/planetary/apod?api_key=H0PSZTXODnpFgc2VU5Xxsh06bGam11sQsPSzpWh0').then( response => {
-    //     this.todaysPlanets = response.data
-    //   })
-    // }
-    getMocks() {
-      this.todaysPlanets = {
-        title: 'sometitle',
-        hdurl: 'image',
-        description: 'lorem ipsum'
-      }
+  export default {
+    name: "App",
+    components: {},
+    data() {
+      return {
+        planets: {},
+      };
     },
-    monthly() {
-      this.daily = false
+    created: function() {
+      // this.getPlanet()
+      this.getMocks();
+    },
+    methods: {
+      // getPlanet() {
+      //   axios.get('https://api.nasa.gov/planetary/apod?api_key=H0PSZTXODnpFgc2VU5Xxsh06bGam11sQsPSzpWh0').then( response => {
+      //     this.planets = response.data
+      //   })
+      // }
+      getMocks() {
+        this.planets = {
+          title: "sometitle",
+          hdurl: "image",
+          description: "lorem ipsum"
+        };
+      },
+      generateTimeStamp() {
+        const currentdate = new Date();
+        const timeStamp =
+          currentdate.getFullYear() +
+          "-" +
+          currentdate.getMonth() +
+          1 +
+          "-" + '0' +
+          currentdate.getDate()
+          console.log(timeStamp)
+        return timeStamp;
+      },
+      Next() {
+        axios
+          .get(
+            `https://api.nasa.gov/planetary/apod?date=${this.generateTimeStamp()}&api_key=H0PSZTXODnpFgc2VU5Xxsh06bGam11sQsPSzpWh0`
+          )
+          .then(response => {
+            this.planets = response.data;
+          });
+      }
     }
-
-  }
-}
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
 
 <!-- nasa api key H0PSZTXODnpFgc2VU5Xxsh06bGam11sQsPSzpWh0 -->
